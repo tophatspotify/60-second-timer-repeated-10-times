abb04f4ffbfbe140ee4e611dd1b320f7a0ec02b2
tophatspotify <colemedlin2013@gmail.com>
2025-03-07 17:27:21 -0600

    Add files via upload

---
+++ 20240311-201746.py
@@ -0,0 +1,16 @@
+
+    
+import time
+print ("Never gonna give you up")
+time.sleep(1)
+print ("Never gonna let you down")
+time.sleep(1)
+print ("Never gonna run around and desert you")
+time.sleep(1)
+print ("Never gonna make you cry")
+time.sleep(1)
+print ("Never gonna say goodbye")
+time.sleep(1)
+print ("Never gonna tell a lie and hurt you")
+time.sleep(0)
+
